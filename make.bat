613fe33ebb3c203639abd09e17e221579d5d7c45
greyhound <635044633@qq.com>
2019-03-19 13:14:05 +0800

    update

--- make.bat
+++ make.bat
@@ -1,2 +1,18 @@
 @echo off
-@npm run dev
+
+if "%1" == "dev" (
+  @npm run dev
+  goto end
+)
+
+if "%1" == "genlist" (
+  @py -3 ./build/py_gen.py
+  goto end
+)
+
+if "%1" == "ghpage" (
+  @xcopy "dist/ghpage" "dist" /E /Q /Y
+  goto end
+)
+
+:end
